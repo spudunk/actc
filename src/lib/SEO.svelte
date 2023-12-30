122740b480580c9bd252a912398f4825036843cf
Christopher Hicks <hickscme@gmail.com>
2023-12-30 12:24:17 -0800

    SEO

---
+++ src/lib/SEO.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  export let url = String($page.url);
+  export let title = "A Custom Touch Construction - Portland Home Contractor";
+  export let description = "A Custom Touch Construction - Contractor Specializing in Windows & Doors, Siding, Kitchen & Bath - Serving Portland and surrounding areas since 1995";
+  export let img = "";
+  export let domain = "actc.pro";
+  export let type = "website"
+  export let canonical = url;
+</script>
+
+<svelte:head>
+  <!-- HTML Meta Tags -->
+  <title>{title}</title>
+  <meta name="description" content={description} />
+
+  <!-- Facebook Meta Tags -->
+  <meta property="og:url" content={url} />
+  <meta property="og:type" content={type} />
+  <meta property="og:title" content={title} />
+  <meta property="og:description" content={description} />
+  {#if img}
+    <meta property="og:image" content={img} />
+  {/if}
+
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta property="twitter:domain" content={domain} />
+  <meta property="twitter:url" content={url} />
+  <meta name="twitter:title" content={title} />
+  <meta name="twitter:description" content={description} />
+  {#if img}
+    <meta name="twitter:image" content={img} />
+  {/if}
+
+  <link rel="“canonical”" href={canonical} />
+</svelte:head>
