d5145290cd066628c7708589144a5e8f0e261262
Christopher Hicks <hickscme@gmail.com>
2023-12-30 14:48:02 -0800

    style gallery

--- src/lib/Carousel.svelte
+++ src/lib/Carousel.svelte
@@ -9,7 +9,7 @@
       src={image.path}
       alt={image.alt}
       id={image.id}
-      class="h-60 md:h-72 w-auto rounded"
+      class="h-60 md:h-72 lg:h-80 w-auto rounded"
     />
   {/each}
 </div>
