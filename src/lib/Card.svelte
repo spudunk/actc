d0b7db2ba14f9ee4443ae24f050a569b837f6790
Christopher Hicks <hickscme@gmail.com>
2024-01-04 11:46:52 -0800

    updates

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -7,18 +7,25 @@
 <div
   class="min-h-52 h-full w-auto rounded p-4 mb-4 bg-neutral-200 flex flex-col gap-1"
 >
-  {#if image}
-    <img class="self-center h-24" src={image} alt="" />
-  {/if}
   {#if link}
-    <a href={link}>
-      <h2 class="text-2xl font-display text-center">{title}</h2>
-    </a>
+    {#if image}
+      <a class="px-2 py-1 link self-center flex flex-col gap-1" href={link} data-sveltekit-preload-data>
+        <img class="self-center h-24" src={image} alt="" />
+        <h2 class="text-2xl font-display text-center">{title}</h2>
+      </a>
+    {:else}
+      <a class="px-2 py-1 link self-center flex" href={link} data-sveltekit-preload-data>
+        <h2 class="text-2xl font-display text-center">{title}</h2>
+      </a>
+    {/if}
+  {:else if image}
+    <img class="self-center h-24" src={image} alt="" />
+    <h2 class="text-2xl font-display text-center">{title}</h2>
   {:else}
     <h2 class="text-2xl font-display text-center">{title}</h2>
   {/if}
-  <div class="flex flex-col gap-2"><slot /></div>
-  {#if link}
-    <a class=" pt-4 pl-2 text-lg font-bold link self-center" href={link}>About {title}</a>
-  {/if}
+
+  <div class="flex flex-col gap-2">
+    <slot />
+  </div>
 </div>
