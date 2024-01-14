9687fb99289ea41d2608784bb90f3ad55278eddb
Christopher Hicks <hickscme@gmail.com>
2024-01-14 12:54:58 -0800

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
+
+<main>
+  <div class="pt-20 container">
+    <h1 class="text-2xl">{$page.status}: {$page.error?.message}</h1>
+  </div>
+</main>
